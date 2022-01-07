c597e18722d7070d201f2415a433247734e3c68e
Val Hustle <stars@MacBook-Air.local>
2022-01-06 23:54:49 -0500

    modified addProperty function to add to the database new property as its passed from the browser in the new listing page with 14 parameters. Queried to verify functionality, it works.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -167,20 +167,6 @@ exports.getAllReservations = getAllReservations;
   .then ((result)=>result.rows)
 };
 
-  //   return pool
-//     .query(`SELECT properties.*, avg(property_reviews.rating) as avg_rating
-//     FROM properties
-//     JOIN property_reviews ON properties.id = property_id
-//     WHERE city LIKE '%ancouv%'
-//     GROUP BY properties.id
-//     HAVING avg(property_reviews.rating) >=4
-//     ORDER BY cost_per_night
-//     LIMIT $2;`, [options, limit])
-//     .then((result) => result.rows)
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
-// };
 exports.getAllProperties = getAllProperties;
 
 
@@ -189,10 +175,34 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+
+
+const addProperty = (property) => {
+  queryString = `INSERT INTO properties (owner_id, title, description,thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,$10, $11, $12, $13, $14) RETURNING *;`;
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night * 100,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      return result
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }
+
 exports.addProperty = addProperty;
